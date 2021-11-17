6e13e2d86a0deaa0d2d5fd0e53e84882362fbd0b
Billie-K <billiemush@gmail.com>
2021-11-16 16:14:40 -0800

    new modif

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Unittest module for the FileStorage class."""
-
+"""Defines unittests for models/engine/file_storage.py.
+Unittest classes:
+    TestFileStorage_instantiation
+    TestFileStorage_methods
+"""
 import unittest
 from datetime import datetime
 import time
